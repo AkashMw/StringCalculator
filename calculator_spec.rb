3b631a6b71b163f39e56e56cc98b4bc6b878fdec
rails <Akash Rajput>
2024-05-23 19:50:38 +0530

    Added test case to handle a number

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe "Calculator" do
     it "should return 0 for empty string" do
       expect(add('')).to eq(0)
     end
+
+    context 'handle numbers' do
+      it "should return 3 for empty '3'" do
+        expect(add('3')).to eq(3)
+      end
+    end
   end
 end
