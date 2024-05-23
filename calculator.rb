a29f3c13c7532ed77fb8fb0109104f3b30d92c98
rails <Akash Rajput>
2024-05-23 21:45:20 +0530

    Modified Add method to raise exception for negetion numbers

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,20 @@
+require 'pry'
 module Calculator
   def add string
     return 0 if string.empty?
+    raise_if_negative_numbers_present?(string)
     return numbers(string).inject(:+)
   end
 
   def numbers(string)
     string.gsub("\n", delimeter(string)).split(delimeter(string)).map{|str| str.to_i }
+  end
+
+  def raise_if_negative_numbers_present?(string)
+    negative_numbers = numbers(string).select {|num| num < 0}
+    if negative_numbers.any?
+      raise "negative numbers not allowed: #{negative_numbers.join(',')}"
+    end
   end
 
   def delimeter(string)
