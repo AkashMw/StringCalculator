539ee216cacb795c89c53b01e1af57f074fcfdbc
rails <Akash Rajput>
2024-05-23 20:00:50 +0530

    Evaluate comma seperated 2 numbers - Added test to evaluate Add method for comma seperated two numbers

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -12,5 +12,11 @@ RSpec.describe "Calculator" do
         expect(add('3')).to eq(3)
       end
     end
+
+    context 'handle comma seperated 2 numbers' do
+      it "should return 3 for empty '1, 2'" do
+        expect(add("1, 2")).to eq(3)
+      end
+    end
   end
 end
