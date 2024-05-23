6fe57d1cf30dcb6c4966bc59e48bb29fcddb780a
rails <Akash Rajput>
2024-05-23 20:12:40 +0530

    Modified Add method to handle comma seperated two numbers

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,8 @@
 module Calculator
   def add string
     return 0 if string.empty?
-    return string.to_i
+    return string.to_i unless string.include?(',')
+    num_array = string.split(',')
+    return num_array[0].to_i + num_array[1].to_i
   end
 end
