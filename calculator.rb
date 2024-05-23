86fc16be4e60f1649fcfe3d6c71f3feee566dcf4
rails <Akash Rajput>
2024-05-23 20:27:50 +0530

    refactor Add method to handle any amount of numbers

--- calculator.rb
+++ calculator.rb
@@ -2,8 +2,10 @@ module Calculator
   def add string
     return 0 if string.empty?
     return string.to_i unless string.include?(',')
-    num_array = string.split(',').map{|str| str.to_i }
-    num_array.inject(:+)
-    return num_array.inject(:+)
+    return numbers(string).inject(:+)
+  end
+
+  def numbers(string)
+    string.split(',').map{|str| str.to_i }
   end
 end
